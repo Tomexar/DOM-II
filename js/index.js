8e7564d2a94eb3b2ecf38ca7857608e10639a963
Tomexar <tommyexar@gmail.com>
2019-04-09 14:51:18 -0600

    used prevent default on the a tags

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+// prevent default
+
+const atags = document.getElementsByClassName('nav');
+console.log(atags)
+for (var i = 0 ; i < atags.length; i++) {
+    atags[i].addEventListener('click' , function(event){
+        console.log('link was clicked')
+        event.preventDefault();
+        alert('Error!')
+    }); 
+ }
